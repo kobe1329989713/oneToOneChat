62f9a85693052bc27fc665b162f4a5edaec687dc
1329989713@qq.com <1329989713@qq.com>
2021-07-26 18:56:02 +0800

    Initial Commit

---
+++ oneToOneChat1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  oneToOneChat1
+//
+//  Created by xn on 2021/7/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
